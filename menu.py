b03b0ce336e137aa64d4a3c31e46b682e1706bbf
trekswithnick <nicholasbrowningdev@outlook.com>
2025-06-14 13:44:30 +1000

    decided that I want to split the menu

--- menu.py
+++ menu.py
@@ -1,3 +1,28 @@
 class Menu:
+    def __init__(self, title, options):
+        self.title = title
+        self.options = options
+
+class MainMenu(Menu):
+    def __init__(self):
+        pass
+
+class AlbumMenu(Menu):
+    def __init__(self):
+        pass
+
+class SongMenu(Menu):
+    def __init__(self):
+        pass
+
+class ArtistMenu(Menu):
+    def __init__(self):
+        pass
+
+class GenreMenu(Menu):
+    def __init__(self):
+        pass
+
+class MenuManager():
     def __init__(self):
         pass
